80309c4537237cc26cd777d933db0fd8cf2c1eeb
Mohirjon013 <mohirjon26@gmail.com>
2024-10-20 02:01:29 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 import CustomRoutes from './routes'
 
 function App() {
 
   return (
     <>
-    
+      <Navbar/>
       <CustomRoutes/>
     </>
   )
